ae2f480e97d1a322c4db2f280fd030cee985a38b
Matthew Wright <matthewbwright@gmail.com>
2019-03-01 10:24:04 -0700

    adds optimization down to 2 seconds (from 11 seconds)

--- names/names.py
+++ names/names.py
@@ -10,13 +10,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# This looks like a runtime complexity of O(n ** 2) because of the nested for loop.
 duplicates = []
+# for name_1 in names_1:
+#    for name_2 in names_2:
+#        if name_1 == name_2:
+#            duplicates.append(name_1)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
